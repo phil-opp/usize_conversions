1859a903499b7dada592314862a3bc51e0e7c840
Philipp Oppermann <dev@phil-opp.com>
2018-01-17 13:24:12 +0100

    Rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,81 @@
-//! Conversion traits for conversions between `usize` and fixed sized integers.
-//!
-//! This crate provides two traits, `UsizeFrom` and `UsizeInto`, that can be used
-//! similar to the `From` and `Into` traits of the standard library.
+//! Conversion traits and functions for conversions between `usize` and fixed sized integers.
 //!
 //! Warning: The traits are conditionally implemented based on the target pointer width,
 //! so they can make your crate less portable.
 
+#![feature(i128_type)]
 #![warn(missing_docs)]
 
 #![no_std]
 
-/// Additional `from` conversions for `usize`.
-pub trait UsizeFrom<T>: Sized {
+/// Create an usize from a fixed sized integer.
+pub fn usize_from<T>(value: T) -> usize where T: IntoUsize {
+    IntoUsize::into_usize(value)
+}
+
+/// Convert a type to `usize`.
+pub trait IntoUsize: Sized {
     /// Performs the conversion.
-    fn from(value: T) -> Self;
+    fn into_usize(self) -> usize;
 }
 
-/// Additional `into` conversions for `usize`.
-pub trait UsizeInto<T>: Sized {
+/// Convert a type from `usize`.
+pub trait FromUsize: Sized {
     /// Performs the conversion.
-    fn into(self) -> T;
+    fn from_usize(usize) -> Self;
 }
 
-impl<T, U> UsizeInto<T> for U
-    where T: UsizeFrom<U>
-{
-    fn into(self) -> T {
-        T::from(self)
-    }
-}
-
-macro_rules! implement {
-    ($x:ty, $y:ty) => {
-        impl UsizeFrom<$x> for $y {
-            fn from(value: $x) -> Self {
-                value as $y
+macro_rules! impl_into_usize {
+    ($x:ty) => {
+        impl IntoUsize for $x {
+            fn into_usize(self) -> usize {
+                self as usize
             }
         }
     }
 }
 
-#[cfg(target_pointer_width = "128")] implement!(usize, u128);
-#[cfg(target_pointer_width = "128")] implement!(u128, usize);
-#[cfg(target_pointer_width = "128")] implement!(u64, usize);
-#[cfg(target_pointer_width = "128")] implement!(u32, usize);
-#[cfg(target_pointer_width = "128")] implement!(u16, usize);
-#[cfg(target_pointer_width = "128")] implement!(u8, usize);
+macro_rules! impl_from_usize {
+    ($x:ty) => {
+        impl FromUsize for $x {
+            fn from_usize(value: usize) -> Self {
+                value as Self
+            }
+        }
+    }
+}
 
-#[cfg(target_pointer_width = "64")] implement!(usize, u64);
-#[cfg(target_pointer_width = "64")] implement!(u64, usize);
-#[cfg(target_pointer_width = "64")] implement!(u32, usize);
-#[cfg(target_pointer_width = "64")] implement!(u16, usize);
-#[cfg(target_pointer_width = "64")] implement!(u8, usize);
+#[cfg(target_pointer_width = "128")] impl_from_usize!(u128);
+#[cfg(target_pointer_width = "128")] impl_into_usize!(u128);
+#[cfg(target_pointer_width = "128")] impl_into_usize!(u64);
+#[cfg(target_pointer_width = "128")] impl_into_usize!(u32);
+#[cfg(target_pointer_width = "128")] impl_into_usize!(u16);
+#[cfg(target_pointer_width = "128")] impl_into_usize!(u8);
 
-#[cfg(target_pointer_width = "32")] implement!(usize, u32);
-#[cfg(target_pointer_width = "32")] implement!(u32, usize);
-#[cfg(target_pointer_width = "32")] implement!(u16, usize);
-#[cfg(target_pointer_width = "32")] implement!(u8, usize);
+#[cfg(target_pointer_width = "64")] impl_from_usize!(u128);
+#[cfg(target_pointer_width = "64")] impl_from_usize!(u64);
+#[cfg(target_pointer_width = "64")] impl_into_usize!(u64);
+#[cfg(target_pointer_width = "64")] impl_into_usize!(u32);
+#[cfg(target_pointer_width = "64")] impl_into_usize!(u16);
+#[cfg(target_pointer_width = "64")] impl_into_usize!(u8);
 
-#[cfg(target_pointer_width = "16")] implement!(usize, u16);
-#[cfg(target_pointer_width = "16")] implement!(u16, usize);
-#[cfg(target_pointer_width = "16")] implement!(u8, usize);
+#[cfg(target_pointer_width = "32")] impl_from_usize!(u128);
+#[cfg(target_pointer_width = "32")] impl_from_usize!(u64);
+#[cfg(target_pointer_width = "32")] impl_from_usize!(u32);
+#[cfg(target_pointer_width = "32")] impl_into_usize!(u32);
+#[cfg(target_pointer_width = "32")] impl_into_usize!(u16);
+#[cfg(target_pointer_width = "32")] impl_into_usize!(u8);
 
-#[cfg(target_pointer_width = "8")] implement!(usize, u8);
-#[cfg(target_pointer_width = "8")] implement!(u8, usize);
+#[cfg(target_pointer_width = "16")] impl_from_usize!(u128);
+#[cfg(target_pointer_width = "16")] impl_from_usize!(u64);
+#[cfg(target_pointer_width = "16")] impl_from_usize!(u32);
+#[cfg(target_pointer_width = "16")] impl_from_usize!(u16);
+#[cfg(target_pointer_width = "16")] impl_into_usize!(u16);
+#[cfg(target_pointer_width = "16")] impl_into_usize!(u8);
+
+#[cfg(target_pointer_width = "8")] impl_from_usize!(u128);
+#[cfg(target_pointer_width = "8")] impl_from_usize!(u64);
+#[cfg(target_pointer_width = "8")] impl_from_usize!(u32);
+#[cfg(target_pointer_width = "8")] impl_from_usize!(u16);
+#[cfg(target_pointer_width = "8")] impl_from_usize!(u8);
+#[cfg(target_pointer_width = "8")] impl_into_usize!(u8);
